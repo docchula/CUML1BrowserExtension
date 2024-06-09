9960e0efc2604dc1319cc7603c2d4f43c93fbe61
Bebiezaza <Apipunnakul@gmail.com>
2024-06-09 14:24:01 +0700

    separate compatibility check to another js file

--- chrome/js/convert.js
+++ chrome/js/convert.js
@@ -3,15 +3,9 @@
 const tabInfo = await getCurrentTab();
 const link = tabInfo.url;
 const convertButton = document.querySelector('#convert');
-const compatStatus = document.querySelector('#status');
-
-const whitelist = [ "doi.org", "elsevier.com", "www.google.com", "library.md.chula.ac.th", "lww.com", 
-    "mhmedical.com", "sagepub.com", "sciencedirect.com", "springer.com", "tandfonline.com", "onlinelibrary.wiley.com" ];
-const blacklist = [ "chrome://", "google.com", "jahonline.org", "jsad.com", "w3schools.com", "youtube.com" ];
-let compatChecked = false;
+// const compatStatus = document.querySelector('#status');
 
 if (tabInfo.url.indexOf("cuml1.md.chula.ac.th") !== -1) {
-    compatChecked = true
     convertButton.disabled = true;
     convertButton.className = "compat-error";
     convertButton.textContent = "link is already in CUML1";
@@ -32,29 +26,6 @@ if (tabInfo.url.indexOf("cuml1.md.chula.ac.th") !== -1) {
         let newURL = await convertToCUML1(link);
         await chrome.tabs.update({ url: newURL });
     });
-}
-
-if (compatChecked === false) {
-    for (let item of whitelist) {
-        if (link.indexOf(item) !== -1) {
-            compatChecked = true;
-            compatStatus.className = "compat-confirm";
-            compatStatus.textContent = "✅ compatible";
-            break;
-        }
-    }
-}
-
-if (compatChecked === false) {
-    for (let item of blacklist) {
-        if (link.indexOf(item) !== -1) {
-            compatChecked = true;
-            convertButton.textContent = "proceed with caution";
-            compatStatus.className = "compat-error";
-            compatStatus.textContent = "❌ incompatible";
-            break;
-        }
-    }
 }
 
 // helper functions
